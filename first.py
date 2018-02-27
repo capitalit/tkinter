54ee626889bd037b35aadb318b5c7d97547831ae
capitalit <kamil@capitalit.pl>
2018-02-27 23:06:04 +0100

    OFF

--- first.py
+++ first.py
@@ -1,58 +1,153 @@
 from tkinter import *
-root = Tk()
-root.geometry('400x200')
-l=Label(root, text="kalkulator")
-l.pack(side=TOP)
 
-def buttonFunction():
-    print("hello world")
+class Application(Frame):
+    """ Main class for calculator"""
 
-def iCalc(source, side) :
-    storeObj = Frame(source, borderwidth=4, bd=4, bg="gray")
-    storeObj.pack(side=side, expand=YES, fill=BOTH)
-    return storeObj
+    def __init__(self, master):
+        """ Initialise the Frame. """
+        super(Application, self).__init__(master)
+        self.task = ""
+        self.UserIn = StringVar()
+        self.grid()
+        self.create_widgets()
 
-def button(source, side , text, command=None):
-    storeObj = Button(source, text=text, command=command)
-    storeObj.pack(side=side, expand=YES, fill=BOTH)
-    return storeObj
+    def create_widgets(self):
+        """ Create all the buttons for calculator. """
+        # User input stored as an Entry widget.
 
-class app(Frame):
-    def __init__(self):
-        Frame.__init__(self)
-        self.option_add('*Font', 'arial 40 bold')
-        self.pack(expand=YES, fill=BOTH)
-        self.master.title('Kalkulator')
+        self.user_input = Entry(self, relief=FLAT,bg = "#5BC8AC", bd = 29,
+                                insertwidth = 4, width = 24,
+                                font = ("Verdana", 20, "bold"), textvariable = self.UserIn, justify = RIGHT)
+        self.user_input.grid(columnspan = 4)
+
+        self.user_input.insert(0, "0")
+
+        # Button for value 7
+        self.button1 = Button(self, relief=FLAT,bg = "#98DBC6", bd = 12,
+                              text = "7", padx = 33, pady = 25, font = ("Helvetica", 40, "bold"),
+                              command = lambda : self.buttonClick(7))
+        self.button1.grid(row = 2, column = 0, sticky = W)
+
+        # Button for value 8
+        self.button2 = Button(self,relief=FLAT, bg = "#98DBC6", bd = 12,
+                              text = "8",  padx = 35, pady = 25,
+                              command = lambda : self.buttonClick(8), font = ("Helvetica", 40, "bold"))
+        self.button2.grid(row = 2, column = 1, sticky = W)
+
+        # Button for value 9
+        self.button3 = Button(self, relief=FLAT,bg = "#98DBC6", bd = 12,
+                              text = "9",  padx = 33, pady = 25,
+                              command = lambda : self.buttonClick(9), font = ("Helvetica", 40, "bold"))
+        self.button3.grid(row = 2, column = 2, sticky = W)
+
+        # Button for value 4
+        self.button4 = Button(self,relief=FLAT, bg = "#98DBC6", bd = 12,
+                              text = "4",  padx = 33, pady = 25,
+                              command = lambda : self.buttonClick(4), font = ("Helvetica", 40, "bold"))
+        self.button4.grid(row = 3, column = 0, sticky = W)
+
+        # Button for value 5
+        self.button5 = Button(self,relief=FLAT, bg = "#98DBC6", bd = 12,
+                              text = "5",  padx = 35, pady = 25,
+                              command = lambda : self.buttonClick(5), font = ("Helvetica", 40, "bold"))
+        self.button5.grid(row = 3, column = 1, sticky = W)
+
+        # Button for value 6
+        self.button6 = Button(self,relief=FLAT, bg = "#98DBC6", bd = 12,
+                              text = "6",  padx = 33, pady = 25,
+                              command = lambda : self.buttonClick(6), font = ("Helvetica", 40, "bold"))
+        self.button6.grid(row = 3, column = 2, sticky = W)
+
+        # Button for value 1
+        self.button7 = Button(self,relief=FLAT, bg = "#98DBC6", bd = 12,
+                              text = "1",  padx = 33, pady = 25,
+                              command = lambda : self.buttonClick(1), font = ("Helvetica", 40, "bold"))
+        self.button7.grid(row = 4, column = 0, sticky = W)
+
+        # Button for value 2
+        self.button8 = Button(self,relief=FLAT, bg = "#98DBC6", bd = 12,
+                              text = "2",  padx = 35, pady = 25,
+                              command = lambda : self.buttonClick(2), font = ("Helvetica", 40, "bold"))
+        self.button8.grid(row = 4, column = 1, sticky = W)
+
+        # Button for value 3
+        self.button9 = Button(self, relief=FLAT,bg = "#98DBC6", bd = 12,
+                              text = "3",  padx = 33, pady = 25,
+                              command = lambda : self.buttonClick(3), font = ("Helvetica", 40, "bold"))
+        self.button9.grid(row = 4, column = 2, sticky = W)
+
+        # Button for value 0
+        self.button9 = Button(self, relief=FLAT,bg = "#98DBC6", bd = 12,
+                              text = "0",  padx = 33, pady = 25,
+                              command = lambda : self.buttonClick(0), font = ("Helvetica", 40, "bold"))
+        self.button9.grid(row = 5, column = 0, sticky = W)
+
+        # Operator buttons
+        # Addition button
+        self.Addbutton = Button(self, relief=FLAT, bg = "#98DBC6", bd = 12,
+                                text = "+",  padx = 36, pady = 25,
+                                command = lambda : self.buttonClick("+"), font = ("Helvetica", 40, "bold"))
+        self.Addbutton.grid(row = 2, column = 3, sticky = W)
+
+        # Subtraction button
+        self.Subbutton = Button(self,relief=FLAT, bg = "#98DBC6", bd = 12,
+                                text = "-",  padx = 39, pady = 25,
+                                command = lambda : self.buttonClick("-"), font = ("Helvetica", 40, "bold"))
+        self.Subbutton.grid(row = 3, column = 3, sticky = W)
+
+        # Multiplication button
+        self.Multbutton = Button(self, relief=FLAT,bg = "#98DBC6", bd = 12,
+                                 text = "*",  padx = 38, pady = 25,
+                                 command = lambda : self.buttonClick("*"), font = ("Helvetica", 40, "bold"))
+        self.Multbutton.grid(row = 4, column = 3, sticky = W)
+
+        # Division button
+        self.Divbutton = Button(self, relief=FLAT,bg = "#98DBC6", bd = 12,
+                                text = "/",  padx = 39, pady = 25,
+                                command = lambda : self.buttonClick("/"), font = ("Helvetica", 40, "bold"))
+        self.Divbutton.grid(row = 5, column = 3, sticky = W)
+
+        # Equal button
+        self.Equalbutton = Button(self,relief=FLAT, bg = "#E6D72A", bd = 12,
+                                  text = "=",  padx = 100, pady = 25,
+                                  command = self.CalculateTask, font = ("Helvetica", 40, "bold"))
+        self.Equalbutton.grid(row = 5, column = 1, sticky = W, columnspan = 2)
+
+        # Clear Button
+        self.Clearbutton = Button(self,relief=FLAT, bg = "#E6D72A", bd = 12,
+                                  text = "GAME OVER", font = ("Helvetica", 44, "bold"), width = 28, padx = 7, command = self.ClearDisplay)
+        self.Clearbutton.grid(row = 1, columnspan = 4, sticky = W)
+
+    def buttonClick(self, number):
+        self.task = str(self.task) + str(number)
+        self.UserIn.set(self.task)
+
+    def CalculateTask(self):
+        self.data = self.user_input.get()
+        try:
+            self.answer = eval(self.data)
+            self.displayText(self.answer)
+            self.task = self.answer
+
+        except SyntaxError as e:
+            self.displayText("niepoprawna składnia!")
+            self.task = ""
+
+    def displayText(self, value):
+        self.user_input.delete(0, END)
+        self.user_input.insert(0, value)
+
+    def ClearDisplay(self):
+        self.task = ""
+        self.user_input.delete(0, END)
+        self.user_input.insert(0, "0")
 
 
-        display = StringVar()
-        Entry(self, relief=FLAT,
-              textvariable=display,justify='right',bd=30 ,bg="white").pack(side=TOP, expand=YES,
-                                                                         fill=BOTH)
-        for clearBut in (["CE"] , ["C"]):
-            erase = iCalc(self ,TOP)
-            for ichar in clearBut:
-                button(erase, LEFT, ichar,
-                       lambda storeObj=display,q=ichar: storeObj.set(''))
+calculator = Tk()
 
+calculator.title("Kalkulator")
+app = Application(calculator)
+# Make window fixed (cannot be resized)
+calculator.resizable(width = False, height = False)
 
-        for Numbut in ("789/" , "456*" , "123-", "0.+"):
-            FunctionNum = iCalc(self, TOP)
-            for char in Numbut:
-                button(FunctionNum,LEFT,char,
-                       lambda storeObj=display, q=char: storeObj.set(storeObj.get() +q))
-
-        EqualsButton = iCalc(self, TOP)
-        for iEquals in "=":
-            if iEquals == '=':
-                btniEquals = button(EqualsButton, LEFT, iEquals)
-                btniEquals.bind('<ButtonRelease-1>',
-                                lambda e, s=self, storeObj=display: s.calc(storeObj),'+')
-            else:
-                btniEquals = button(EqualsButton, LEFT, iEquals,
-                                    lambda storeObj=display, s=' %s '%iEquals: storeObj.set(storeObj.get()+s))
-
-
-
-if __name__== '__main__':
-    app().mainloop()
+calculator.mainloop()
